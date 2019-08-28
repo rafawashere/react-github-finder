440ee760dfb071b92986742e8897633055b0766a
Rafael Hernandez <rafaelh@rafaelh.us>
2019-08-28 11:05:12 -0700

    Revert "Revert "Create searchbar and results layout""
    This reverts commit e3843e9f0c25889dfb54a6dc1f658127f51cb254.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    
+    // this.setState({ loading: true });
+    // const res =  await axios.get(`https://api.github.com/users?client_id=$
+    //   {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    //   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}
+    //   `);
+    // this.setState({
+    //   users: res.data,
+    //   loading: false
+    // });
+  }
+
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({loading: true});
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: res.data.items,
+      loading: false
+    });
+  }
+
+  // Clear Github users form state
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    })
+  }
+
   render() {
+
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers}showClear={ users.length > 0 ? true : false }/>
+          <Users loading={loading} users={users} />
+        </div>
       </div>
     );
   }

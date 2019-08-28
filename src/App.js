2d42e2293187656a002909e4c2417956eeaa6a5a
Rafael Hernandez <rafaelh@rafaelh.us>
2019-08-28 15:51:17 -0700

    complete app functionality
    At this step we have completed the app functionality. Next is to refactor applications to use funtions istead of classes. App state is stable.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -58,6 +59,20 @@ class App extends Component {
     });
   };
 
+  // Get single Github user repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      repos: res.data,
+      loading: false
+    });
+  };
+
   // Clear Github users form state
   clearUsers = () => {
     this.setState({
@@ -78,7 +93,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -111,7 +126,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}

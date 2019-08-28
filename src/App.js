85475bb273659fe84bea8be01ed6a5e763c71718
Rafael Hernandez <rafaelh@rafaelh.us>
2019-08-28 14:48:01 -0700

    Create User Profiles
    Create basic user profile page of Github profile.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import About from "./components/pages/About";
 import axios from "axios";
@@ -12,6 +13,7 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -42,10 +44,25 @@ class App extends Component {
     });
   };
 
+  // Get single Github user
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      user: res.data,
+      loading: false
+    });
+  };
+
   // Clear Github users form state
   clearUsers = () => {
     this.setState({
       users: [],
+      user: {},
       loading: false
     });
   };
@@ -61,7 +78,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -87,6 +104,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const UserItem = ({user: {login, avatar_url, html_url }}) => {
-  
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className="card text-center">
       <img
         src={avatar_url}
         alt="octocat"
         className="round-img"
-        style={{ width: '60px' }}
+        style={{ width: "60px" }}
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-          </a>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 UserItem.propTypes = {
   user: PropTypes.object.isRequired
+};
 
-}
-
-export default UserItem
+export default UserItem;
